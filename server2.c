339490d0eec3cb7987cba7830661756b97cdbeaf
khaterdor <156497069+khaterdor@users.noreply.github.com>
2024-09-08 08:15:35 +0530

    Create server2.c

---
+++ server2.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define BUFFER_SIZE 1024
+
+void error(const char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+void send_command(int sockfd, const char *cmd, char *response) {
+    send(sockfd, cmd, strlen(cmd), 0);
+    recv(sockfd, response, BUFFER_SIZE, 0);
+}
+
+void ftp_send_file(int sockfd, const char *filename) {
+    FILE *file = fopen(filename, "rb");
+    if (!file) {
+        error("Error opening file");
+    }
+
+    char buffer[BUFFER_SIZE];
+    size_t n;
+
+    while ((n = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+        send(sockfd, buffer, n, 0);
+    }
+
+    fclose(file);
+}
+
+void ftp_receive_file(int sockfd, const char *filename) {
+    FILE *file = fopen(filename, "wb");
+    if (!file) {
+        error("Error opening file");
+    }
+
+    char buffer[BUFFER_SIZE];
+    ssize_t n;
+
+    while ((n = recv(sockfd, buffer, sizeof(buffer), 0)) > 0) {
+        fwrite(buffer, 1, n, file);
+    }
+
+    fclose(file);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 6) {
+        fprintf(stderr, "Usage: %s <server_ip> <port> <username> <password> <file_to_send> <file_to_receive>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    const char *server_ip = argv[1];
+    int port = atoi(argv[2]);
+    const char *username = argv[3];
+    const char *password = argv[4];
+    const char *file_to_send = argv[5];
+    const char *file_to_receive = argv[6];
+
+    int sockfd;
+    struct sockaddr_in server_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        error("Error opening socket");
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    inet_pton(AF_INET, server_ip, &server_addr.sin_addr);
+
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        error("Error connecting");
+    }
+
+    char response[BUFFER_SIZE];
+    
+    // Login
+    send_command(sockfd, "USER ", response);
+    send_command(sockfd, username, response);
+    send_command(sockfd, "\r\n", response);
+
+    send_command(sockfd, "PASS ", response);
+    send_command(sockfd, password, response);
+    send_command(sockfd, "\r\n", response);
+
+    // Send file
+    send_command(sockfd, "STOR ", response);
+    send_command(sockfd, file_to_send, response);
+    send_command(sockfd, "\r\n", response);
+
+    ftp_send_file(sockfd, file_to_send);
+
+    // Receive file
+    send_command(sockfd, "RETR ", response);
+    send_command(sockfd, file_to_receive, response);
+    send_command(sockfd, "\r\n", response);
+
+    ftp_receive_file(sockfd, file_to_receive);
+
+    close(sockfd);
+    return 0;
+}
